fde154051dbfb9ee0bb69ef80493c9b7c67bf690
Nikita Akosnov <nikitaua4226@gmail.com>
2024-02-14 21:00:55 +0200

    initial commit

---
+++ src/main/java/com/unbidden/jvtaskmanagementsystem/JvTaskManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.unbidden.jvtaskmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvTaskManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvTaskManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unbidden/jvtaskmanagmentsystem/JvTaskManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unbidden.jvtaskmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvTaskManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
