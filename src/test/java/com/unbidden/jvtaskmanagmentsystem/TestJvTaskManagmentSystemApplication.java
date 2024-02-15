de040dd1a6725ac1b90ad6f901ec93de8ef19f2d
Nikita Akosnov <nikitaua4226@gmail.com>
2024-02-15 18:10:04 +0200

    resolved checkstyle issues

--- src/test/java/com/unbidden/jvtaskmanagmentsystem/TestJvTaskManagmentSystemApplication.java
+++ src/test/java/com/unbidden/jvtaskmanagmentsystem/TestJvTaskManagmentSystemApplication.java
@@ -1,15 +1,15 @@
 package com.unbidden.jvtaskmanagmentsystem;
 
 import com.unbidden.jvtaskmanagementsystem.JvTaskManagmentSystemApplication;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestJvTaskManagmentSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(JvTaskManagmentSystemApplication::main).with(TestJvTaskManagmentSystemApplication.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(JvTaskManagmentSystemApplication::main)
+                .with(TestJvTaskManagmentSystemApplication.class).run(args);
+    }
 
 }
